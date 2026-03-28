635dbbc27b355f86478ebb3638c333b2fd4072f4
yopiyep <artemkol200831@gmail.com>
2026-03-28 14:59:23 +0700

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,29 +1,14 @@
-def bm_find(s, sub, start=0, end=None, all_=False):
-    end = len(s) if end is None else min(end, len(s))
-    start = max(0, start)
-
-    if sub == "":
-        return ",".join(map(str, range(start, end + 1))) if all_ else start
-
-    m = len(sub)
-    if m > end - start:
-        return "" if all_ else -1
-
-    bad = {c: i for i, c in enumerate(sub)}
+def find_all(s: str, sub: str):
     res = []
-    i = start
+    n, m = len(s), len(sub)
 
-    while i <= end - m:
-        j = m - 1
-        while j >= 0 and s[i + j] == sub[j]:
-            j -= 1
-
-        if j < 0:
-            if not all_:
-                return i
+    for i in range(n - m + 1):
+        match = True
+        for j in range(m):
+            if s[i + j] != sub[j]:
+                match = False
+                break
+        if match:
             res.append(str(i))
-            i += 1
-        else:
-            i += max(1, j - bad.get(s[i + j], -1))
 
-    return ",".join(res) if all_ else -1
+    return ",".join(res)
